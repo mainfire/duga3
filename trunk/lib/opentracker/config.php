2e4d4df23fc6629a306d8371987824034ced269f
somicide <somicide@59b0470c-bcfb-476b-b030-389405ee84ab>
2010-06-18 20:39:56 +0000

    Clarify that this new list system is 100% compatible with erdgeist's opentracker software

--- trunk/lib/opentracker/config.php
+++ trunk/lib/opentracker/config.php
@@ -41,6 +41,6 @@ define('ANNOUNCE_RETURN','100'); #our default amount of peers to return on an an
 define('FULLSCRAPE','2'); #fullscrape type: 0 for false (force to ask for specific infohashes), 1 for true, 2 for true with gzip
 define('SCRAPE_INTERVAL','60'); #value (in minutes) that is added onto the above ANNOUNCE_INTERVAL, you should make this double of the original value
 
-define('LISTLOCATION','C:\web\blacklist.txt'); #location of the below list file (example: /usr/home/kipz/blacklist or C:\web\blacklist.txt)
+define('LISTLOCATION','C:\web\blacklist.txt'); #location of the below list type file, this is 100% compatible with erdgeists opentracker software (example: /usr/home/kipz/blacklist or C:\web\blacklist.txt)
 define('LISTTYPE','blacklist'); #blacklist (allow all torrents except the listed hashes), whitelist (block all torrents except the listed hashes), anything else will disable both
 ?>
