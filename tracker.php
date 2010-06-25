78b8ef91bf7985ae78ec12c5c7c9f1739f81d9de
somicide <somicide@59b0470c-bcfb-476b-b030-389405ee84ab>
2010-06-25 07:10:28 +0000

    Check if tracker is installed on stats page, try / catch brackets, cleaning some strange trailing whitespace

--- tracker.php
+++ tracker.php
@@ -2,6 +2,14 @@
 require_once "lib/opentracker/config.php";
 require_once "lib/opentracker/functions.php";
 $torrent = (isset($_GET['torrent'])) ? $_GET['torrent'] : null;
+try
+{
+	$db = new mysqli(MYSQLSERVER,MYSQLNAME,MYSQLPASSWORD,MYSQLBASE);
+	if (!$db->query("select id from announce limit 1") || !$db->query("select id from history limit 1"))
+	{
+		$db->query($announce);
+		$db->query($history);
+	}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
 "http://www.w3.org/TR/REC-html40/loose.dtd">
@@ -73,27 +81,26 @@ $torrent = (isset($_GET['torrent'])) ? $_GET['torrent'] : null;
 		<h1>open tracker</h1>
 		<div id="output">
 <?php
-$db = new mysqli(MYSQLSERVER,MYSQLNAME,MYSQLPASSWORD,MYSQLBASE);
-$timestamp = time();
-if (!is_null($torrent))
-{
-	if (strlen($torrent) == 40)
+	$timestamp = time();
+	if (!is_null($torrent))
 	{
-		$query1 = $db->query("select * from history where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) limit 1");
-		if ($query1->num_rows > 0)
+		if (strlen($torrent) == 40)
 		{
+			$query1 = $db->query("select * from history where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) limit 1");
+			if ($query1->num_rows > 0)
+			{
 ?>
 		<h3><?php print $torrent; ?></h3>
 <?php
-			$query2 = $db->query("select * from announce where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) order by timestamp desc limit 200");
-			if ($query2->num_rows > 0)
-			{
-				$query3 = $db->query("select sum(incomplete) as leechs, sum(complete) as seeds, sum(downloaded) as snags from history where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) limit 1");
-				while ($line = $query3->fetch_object())
+				$query2 = $db->query("select * from announce where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) order by timestamp desc limit 200");
+				if ($query2->num_rows > 0)
 				{
-					$seeds = intval($line->seeds,0);
-					$leechs = intval($line->leechs,0);
-					$snags = intval($line->snags,0);
+					$query3 = $db->query("select sum(incomplete) as leechs, sum(complete) as seeds, sum(downloaded) as snags from history where match (hash) against ('\"$torrent\"' IN BOOLEAN MODE) limit 1");
+					while ($line = $query3->fetch_object())
+					{
+						$seeds = intval($line->seeds,0);
+						$leechs = intval($line->leechs,0);
+						$snags = intval($line->snags,0);
 ?>
 			<h3>swarm totals</h3>
 			<ul>
@@ -102,35 +109,45 @@ if (!is_null($torrent))
 				<li id="enable">downloads: <?php echo $snags; ?></li>
 			</ul>
 <?php
-				}
+					}
 ?>
 			<h3>swarm information</h3>
 			<ul>
 <?php
-				while ($line = $query2->fetch_object())
-				{
-					$uploaded = format_bytes($line->uploaded);
-					$downloaded = format_bytes($line->downloaded);
-					$event = $line->event;
-					$left = $line->remain;
-					$peerid = explode('-',stripslashes($line->peerid));
-					$xes = null;
-					for ($i = 0; $i < strlen($peerid[2]); ++$i)
+					while ($line = $query2->fetch_object())
 					{
-						$xes .= "x";
-					}
+						$uploaded = format_bytes($line->uploaded);
+						$downloaded = format_bytes($line->downloaded);
+						$event = $line->event;
+						$left = $line->remain;
+						$peerid = explode('-',stripslashes($line->peerid));
+						$xes = null;
+						for ($i = 0; $i < strlen($peerid[2]); ++$i)
+						{
+							$xes .= "x";
+						}
 ?>
 				<li><?php print $peerid[1].'-'.$xes;?>: <em><?php print $event; ?></em> with <?php print $downloaded; ?> downloaded and <?php print $uploaded; ?> uploaded (<?php print $left; ?> pieces left)</li>
 <?php
-				}
+					}
 ?>
 			</ul>
 <?php
+				}
+				else
+				{
+?>
+		it seems that this torrent has become stale (no peers) and will expire shortly
+		<br />
+		<br />
+<?php
+				}
 			}
 			else
 			{
 ?>
-		it seems that this torrent has become stale (no peers) and will expire shortly
+		<h3>invalid torrent</h3>
+		the infohash <?php print $torrent; ?> does not exist
 		<br />
 		<br />
 <?php
@@ -139,41 +156,31 @@ if (!is_null($torrent))
 		else
 		{
 ?>
-		<h3>invalid torrent</h3>
-		the infohash <?php print $torrent; ?> does not exist
-		<br />
-		<br />
-<?php
-		}
-	}
-	else
-	{
-?>
 		<h3>invalid torrent infohash</h3>
 		torrent infohash must exist on the tracker already, and must be 40 bytes long (consisting of the alphanumerics A-F/0-9)
 		<br />
 		<br />
 <?php
-	}
+		}
 ?>
 		<a href="?">return</a>...
 <?php
-}
-else
-{
-	$blacklisted = count(explode('\n',file_get_contents("lib/opentracker/blacklist.txt")));
-	$db->query("delete from announce where expire < $timestamp");
-	$db->query("delete from history where expire < $timestamp");
-	$query1 = $db->query("select count(id) as torrents, sum(incomplete) as leechs, sum(complete) as seeds, sum(downloaded) as snags from history");
-	$query2 = $db->query("select * from announce order by timestamp desc limit 25");
-	if ($query1->num_rows > 0)
+	}
+	else
 	{
-		while ($line = $query1->fetch_object())
+		$blacklisted = count(explode('\n',file_get_contents("lib/opentracker/blacklist.txt")));
+		$db->query("delete from announce where expire < $timestamp");
+		$db->query("delete from history where expire < $timestamp");
+		$query1 = $db->query("select count(id) as torrents, sum(incomplete) as leechs, sum(complete) as seeds, sum(downloaded) as snags from history");
+		$query2 = $db->query("select * from announce order by timestamp desc limit 25");
+		if ($query1->num_rows > 0)
 		{
-			$torrents = intval($line->torrents,0);
-			$seeds = intval($line->seeds,0);
-			$leechs = intval($line->leechs,0);
-			$snags = intval($line->snags,0);
+			while ($line = $query1->fetch_object())
+			{
+				$torrents = intval($line->torrents,0);
+				$seeds = intval($line->seeds,0);
+				$leechs = intval($line->leechs,0);
+				$snags = intval($line->snags,0);
 ?>
 			<h3>tracker totals</h3>
 			<ul>
@@ -184,42 +191,47 @@ else
 				<li id="enable">downloads: <?php echo $snags; ?></li>
 			</ul>
 <?php
+			}
 		}
-	}
-	if ($query2->num_rows > 0)
-	{
+		if ($query2->num_rows > 0)
+		{
 ?>
 		<h3>last 25 announce actions</h3>
 		<ul>
 <?php
-		while ($line = $query2->fetch_object())
-		{
-			$event = $line->event;
-			$hash = $line->hash;
-			$peerid = explode('-',stripslashes($line->peerid));
-			$timestamp = date("m/d/Y \@ H:\x:\x",$line->timestamp);
-			$xes = null;
-			for ($i = 0; $i < strlen($peerid[2]); ++$i)
+			while ($line = $query2->fetch_object())
 			{
-				$xes .= "x";
-			}
+				$event = $line->event;
+				$hash = $line->hash;
+				$peerid = explode('-',stripslashes($line->peerid));
+				$timestamp = date("m/d/Y \@ H:\x:\x",$line->timestamp);
+				$xes = null;
+				for ($i = 0; $i < strlen($peerid[2]); ++$i)
+				{
+					$xes .= "x";
+				}
 ?>
 			<li id="enable"><em><?php print $peerid[1].'-'.$xes;?></em> <?php print $event; ?> <strong><a href="?torrent=<?php print $hash; ?>"><?php print $hash; ?></a></strong> on <?php print $timestamp; ?></li>
 <?php
-		}
+			}
 ?>
 		</ul>
 <?php
-	}
-	else
-	{
+		}
+		else
+		{
 ?>
 		<h3>nothing happening</h3>
 		no clients or torrents to show
 <?php
+		}
 	}
+	$db->close();
 }
-$db->close();
+catch(Exception $e)
+{
+	errorexit("tracker is down!");
+}
 ?>
 		</div>
 	</body>
