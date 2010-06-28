59ec491a98c03e7af0455ecc01cab0d6e1c24a92
somicide <somicide@59b0470c-bcfb-476b-b030-389405ee84ab>
2010-06-28 08:39:07 +0000

    New cleaning method - run lib/opentracker/clean.php from cron every 30 or so minutes to clean the history correctly (required)

---
+++ trunk/lib/opentracker/clean.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'config.php';
+try
+{
+	if ($_SERVER['REMOTE_ADDR'] == CLEAN_IP)
+	{
+		$timestamp = time();
+		$db = new mysqli(MYSQLSERVER,MYSQLNAME,MYSQLPASSWORD,MYSQLBASE);
+		$deadswarm = $db->query("select * from announce where expire < $timestamp");
+		if ($deadswarm->num_rows > 0)
+		{
+			while ($line = $deadswarm->fetch_object())
+			{
+				$hash = $line->hash;
+				$id = $line->id;
+				$left = $line->remain;
+				if ($left != 0)
+				{
+					$db->query("update history set incomplete = incomplete - 1 where match (hash) against ('\"$hash\"' IN BOOLEAN MODE) limit 1");
+				}
+				else
+				{
+					$db->query("update history set complete = complete - 1 where match (hash) against ('\"$hash\"' IN BOOLEAN MODE) limit 1");
+				}
+				$db->query("delete from announce where id = $id limit 1");
+			}
+		}
+		$db->query("delete from history where expire < $timestamp");
+		$db->query("optimize table announce");
+		$db->query("optimize table history");
+		$db->close();
+	}
+}
+catch(Exception $e)
+{
+	die($e);
+}
+?>
